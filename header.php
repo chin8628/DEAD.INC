38bb4eae01b8e932ef647e641fba3bbc8f0d37e9
Boonyarith Piriyothinkul <boon.8628@hotmail.com>
2015-11-27 16:22:26 +0700

    fix bug

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <title>DEAD .INC</title>
 
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="asset/css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="asset/css/bootstrap.min.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="asset/css/style.css" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
