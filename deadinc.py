8d05f6d37606ddae7ea2db10cd2f8a962a79fc66
chin8628 <boon.8628@gmail.com>
2015-12-05 19:53:09 +0700

    Generate url to graph finished

--- deadinc.py
+++ deadinc.py
@@ -1,5 +1,7 @@
 import web
 from web import form
+from convert_country import *
+
 
 render = web.template.render('templates/', base='layout')
 
@@ -20,7 +22,15 @@ class Index:
 class Result:
     def GET(self, country):
         image = country.lower() + ".svg"
-        return render.result(country, image)
+        return render.result({
+                                "title": country.replace("-", " ").capitalize(),
+                                "image": image,
+                                "url_to_graph_crude": "../static/svg/" +  full_to_short_country(country.replace(" ", "-")) + "_Death_rate_crude_(per_1000_people).svg",
+                                "url_to_graph_under_five": "../static/svg/" +  full_to_short_country(country.replace(" ", "-")) + "_Number_of_under-five_deaths.svg",
+                                "url_to_graph_injury": "../static/svg/" +  full_to_short_country(country.replace(" ", "-")) + "_Cause_of_death_by_injury_(%_of_total).svg",
+                                "url_to_graph_battle": "../static/svg/" +  full_to_short_country(country.replace(" ", "-")) + "_Battle-related_deaths_(number_of_people).svg",
+                                "url_to_graph_population": "../static/svg/" +  full_to_short_country(country.replace(" ", "-")) + "_Population_total.svg"
+                            })
 
 class Country:
     def GET(self, name=None):
