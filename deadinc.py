c277c470210f375eb8f934983986ee570f50ca3f
chin8628 <boon.8628@gmail.com>
2015-12-07 22:19:55 +0700

    Fix bug
    return convert_country.py and change method to covert country's code for
    call graph, map-world

--- deadinc.py
+++ deadinc.py
@@ -27,12 +27,12 @@ class Result:
         return render.result({
                                 "title": country.replace("-", " ").capitalize(),
                                 "image": image,
-                                "url_to_graph_crude": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Death_rate_crude_(per_1000_people).svg",
-                                "url_to_graph_under_five": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Number_of_under-five_deaths.svg",
-                                "url_to_graph_injury": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Cause_of_death_by_injury_(%_of_total).svg",
-                                "url_to_graph_battle": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Battle-related_deaths_(number_of_people).svg",
-                                "url_to_graph_population": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Population_total.svg",
-                                "country_code_2_letter": countries.get((country.replace("-", " ")))[1]
+                                "url_to_graph_crude": "../static/svg/" +  full_to_short_country((country.replace(" ", "-"))).lower() + "_Death_rate_crude_(per_1000_people).svg",
+                                "url_to_graph_under_five": "../static/svg/" +  full_to_short_country((country.replace(" ", "-"))).lower() + "_Number_of_under-five_deaths.svg",
+                                "url_to_graph_injury": "../static/svg/" +  full_to_short_country((country.replace(" ", "-"))).lower() + "_Cause_of_death_by_injury_(%_of_total).svg",
+                                "url_to_graph_battle": "../static/svg/" +  full_to_short_country((country.replace(" ", "-"))).lower() + "_Battle-related_deaths_(number_of_people).svg",
+                                "url_to_graph_population": "../static/svg/" +  full_to_short_country((country.replace(" ", "-"))).lower() + "_Population_total.svg",
+                                "country_code_2_letter": countries.get(full_to_short_country((country.replace(" ", "-"))))[1]
                             })
 
 class Country:
