6a29a5930a4f8b39b8a99458ace57fa4124f03c5
NBZero <nbzero0@gmail.com>
2015-12-07 17:02:01 +0700

    Fix Character-case bug on graph

--- deadinc.py
+++ deadinc.py
@@ -27,11 +27,11 @@ class Result:
         return render.result({
                                 "title": country.replace("-", " ").capitalize(),
                                 "image": image,
-                                "url_to_graph_crude": "../static/svg/" +  countries.get((country.replace("-", " ")))[2] + "_Death_rate_crude_(per_1000_people).svg",
-                                "url_to_graph_under_five": "../static/svg/" +  countries.get((country.replace("-", " ")))[2] + "_Number_of_under-five_deaths.svg",
-                                "url_to_graph_injury": "../static/svg/" +  countries.get((country.replace("-", " ")))[2] + "_Cause_of_death_by_injury_(%_of_total).svg",
-                                "url_to_graph_battle": "../static/svg/" +  countries.get((country.replace("-", " ")))[2] + "_Battle-related_deaths_(number_of_people).svg",
-                                "url_to_graph_population": "../static/svg/" +  countries.get((country.replace("-", " ")))[2] + "_Population_total.svg",
+                                "url_to_graph_crude": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Death_rate_crude_(per_1000_people).svg",
+                                "url_to_graph_under_five": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Number_of_under-five_deaths.svg",
+                                "url_to_graph_injury": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Cause_of_death_by_injury_(%_of_total).svg",
+                                "url_to_graph_battle": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Battle-related_deaths_(number_of_people).svg",
+                                "url_to_graph_population": "../static/svg/" +  countries.get((country.replace("-", " ")))[2].lower() + "_Population_total.svg",
                                 "country_code_2_letter": countries.get((country.replace("-", " ")))[1]
                             })
 
